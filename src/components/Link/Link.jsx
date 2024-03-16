c88350d9cce32020811993bfcbacb1b980797bfa
hironmoy-ray <hpray35@gmail.com>
2024-03-16 10:49:00 +0600

    DropDown Fixed

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -4,7 +4,7 @@ const Link = ({ route }) => {
   const { path, name } = route;
 
   return (
-    <li className="mr-10 px-6 hover:bg-violet-500">
+    <li className="mr-10 px-6 pb-2 hover:bg-violet-500">
       <a href={path}>{name}</a>
     </li>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
+import { GiSplitCross } from "react-icons/gi";
 import Link from "../Link/Link";
-import { IoMenu } from "react-icons/io5";
-import { RxCross1 } from "react-icons/rx";
 
 const Navbar = () => {
-    const [open,setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   const routes = [
     { id: 1, name: "Home", path: "/" },
     { id: 2, name: "About", path: "/about" },
@@ -15,14 +16,14 @@ const Navbar = () => {
 
   return (
     <nav className="p-4 bg-purple-700 text-white">
-        <div className="text-2xl md:hidden" onClick={()=> setOpen(!open)}>
-            {
-                open === true ? <RxCross1 /> : <IoMenu></IoMenu>
-            }
-        </div>
-      <ul className={`md:flex justify-between bg-purple-700 items-center absolute md:static duration-1000
-      ${open?'top-14':'-top-40'}
-      `}>
+      <div className="text-2xl md:hidden" onClick={() => setOpen(!open)}>
+        {open === true ? <GiSplitCross /> : <GiHamburgerMenu />}
+      </div>
+      <ul
+        className={`md:flex justify-between bg-purple-700 items-center absolute md:static duration-1000
+      ${open ? "top-14" : "-top-40"}
+      `}
+      >
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
         ))}
